abc0eff9a17f36f28ee84dc9165972645f9008f5
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-21 18:36:16 -0300

    checkpoint --> created db.json file, installed axios

--- README.md
+++ README.md
@@ -6,6 +6,19 @@
 npm i -g parcel
 ```
 
+install JSON server (development)
+
+```bash
+npm i -g json-server
+```
+
+start JSON server
+
+```bash
+// in second terminal window
+json-server -w db.json
+```
+
 To install the dependencies
 
 ```bash
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello Typescript!');
+import { User } from './models/User';
+
+const user = new User({name: 'Matt', age: 28});
+
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
+
+user.trigger('change');
+user.trigger('save');
